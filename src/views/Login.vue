367545ce7b40949677584e350ac1604fff3e1e5d
PaulBallestra <paulballestra@yahoo.fr>
2023-03-03 15:32:35 +0100

    :beginner:  Project Init : Login Page - Tabs

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Login</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div style="background-color: #1F1F1F; padding: 36px; border-radius: 5px; margin: 64px auto; max-width: 520px; display: flex; flex-direction: column; justify-content: center; gap: 36px;">
+
+        <ion-input placeholder="Username" style="color: white; border-bottom: 1px solid gray;"></ion-input>
+        <ion-input placeholder="Password" style="color: white; border-bottom: 1px solid gray;"></ion-input>
+        <ion-button>LOGIN</ion-button>
+
+      </div>
+
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/vue';
+</script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,20 +3,16 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+
+        <ion-tab-button tab="login" href="/tabs/login">
+          <ion-icon name="log-in-outline"></ion-icon>
+          <ion-label>Login</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="tab2" href="/tabs/dashboard">
+          <ion-label>Dashboard</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -24,5 +20,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {  } from 'ionicons/icons';
 </script>
